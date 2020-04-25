55e3794ce055a92cb6320982928910cbb0e0a6e6
Stefano Longhi <stefano@moneyfarm.com>
2020-04-25 13:28:47 +0100

    clean

--- src/components/UUIDList.js
+++ src/components/UUIDList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from "react";
+import React from "react";
 import UUIDCard from './uuidCard';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
