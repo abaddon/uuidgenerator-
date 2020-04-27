74f598af86071f47b8b4594ff4372ae68508b3d4
Stefano Longhi <stefano@moneyfarm.com>
2020-04-27 15:52:04 +0100

    updated footer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import packageJson from '../package.json';
+global.appVersion = packageJson.version;
 
 ReactDOM.render(
   <React.StrictMode>
