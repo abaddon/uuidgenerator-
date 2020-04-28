0579da86c2ac249704f1b87cd5acfae94e7c8323
Stefano Longhi <stefano@moneyfarm.com>
2020-04-28 18:44:02 +0100

    added pages lazy loading and webPack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+var CompressionPlugin = require('compression-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
         chunkFilename: '[id].js',
         publicPath: ''
@@ -52,7 +56,26 @@ module.exports = {
             }
         ]
     },
+    optimization: {
+        minimize: true,
+        splitChunks: {
+          name: 'vendor',
+          minChunks: Infinity
+        },
+        minimizer: [
+          new UglifyJsPlugin({
+            cache: false,
+            uglifyOptions: {
+              output: {
+                comments: true
+              }
+            },
+            extractComments: true
+          })
+        ]
+      },
     plugins: [
+        new CleanWebpackPlugin(),
         new CopyWebpackPlugin([
             {
                 from: 'public',
@@ -62,6 +85,18 @@ module.exports = {
             template: __dirname + '/public/index.html',
             filename: 'index.html',
             inject: 'body'
-        })
+        }),
+        new CompressionPlugin({
+            filename: '[path].br[query]',
+            algorithm: 'brotliCompress',
+            test: /\.(js|css|html|svg)$/,
+            compressionOptions: {
+              // zlib’s `level` option matches Brotli’s `BROTLI_PARAM_QUALITY` option.
+              level: 11,
+            },
+            threshold: 10240,
+            minRatio: 0.8,
+            deleteOriginalAssets: false,
+          })
     ]
 };
