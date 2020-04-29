aedb676febb7b76c3cf57d58aa683184301f1e2d
Stefano Longhi <stefano@moneyfarm.com>
2020-04-29 09:54:24 +0100

    added react-helmet to manage title and meta

--- src/pages/ApisDocs.js
+++ src/pages/ApisDocs.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {Helmet} from 'react-helmet'
 import { RedocStandalone } from 'redoc';
 
 
 export default function ApisDocs() {
 
   return (
+      <>
+        <Helmet>
+          <title>Free online UUID generator API</title>
+          <meta name="description" content="Public API used to generate the Universally unique identifier (UUID) version 4" />
+        </Helmet>
         <main>   
           <RedocStandalone specUrl="/openApi3.yml"/>
         </main> 
+      </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {Helmet} from 'react-helmet'
 import UUIDList from '../components/UUIDList';
 
 
 export default function Home() {
 
   return (
+      <>
+        <Helmet>
+          <title>Free online UUID generator</title>
+          <meta name="description" content="This is a free online tool allow you to generate Universally unique identifier (UUID) version 4" />
+        </Helmet>
         <main>   
           <UUIDList/>
         </main> 
+      </>
   );
 }
